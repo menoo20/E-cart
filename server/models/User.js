1ab7be9a52e2a209e25629075a0635fb845c0ff8
mohammed-ameen <menooteaching10@gmail.com>
2022-04-03 09:31:36 +0200

    setup and activate the cryptojs library and hashed my password with it

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,36 @@
 const mongoose = require("mongoose");
+var crypto = require('crypto');
+const res = require("express/lib/response");
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    hash : String, 
+    salt : String ,
     isAdmin: {
         type: Boolean, 
         default: false
     }
 }, {timestamps: true})
+
+
+
+UserSchema.methods.setPassword = function(password) { 
+     
+    // Creating a unique salt for a particular user 
+       this.salt = crypto.randomBytes(16).toString('hex'); 
+     
+       // Hashing user's salt and password with 1000 iterations, 
+        
+       this.hash = crypto.pbkdf2Sync(password, this.salt,  
+       1000, 64, `sha512`).toString(`hex`); 
+   }; 
+     
+   // Method to check the entered password is correct or not 
+   UserSchema.methods.validPassword = function(password) { 
+       var hash = crypto.pbkdf2Sync(password,  
+       this.salt, 1000, 64, `sha512`).toString(`hex`); 
+       return this.hash === hash; 
+   }; 
 
 module.exports = mongoose.model("User", UserSchema)
