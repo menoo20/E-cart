58c760e84ed81223c3fe70da08029db9a13f378c
mohammed-ameen <menooteaching10@gmail.com>
2022-04-03 09:31:36 +0200

    setup and activate the cryptojs library and hashed my password with it

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,13 +8,49 @@ router.post("/register", async (req,res)=>{
         password: req.body.password
     });
 
+    // Call setPassword function to hash password 
+    newUser.setPassword(req.body.password); 
     try{
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+        const savedUser = await newUser.save((err, User)=>{
+          if(err){
+              return res.status(500).send({
+                  message: "Failed to add user"
+              })
+          }else{
+            return  res.status(201).send({
+                  message: "User is added successfully"
+              })
+          }
+        });
     }catch(err){
         res.status(500).json(err);
     }
 })
+
+// User login api 
+router.post('/login', async(req, res) => { 
+
+    // Find user with requested email 
+    await User.findOne({ email : req.body.email }, function(err, user) { 
+        if (user === null) { 
+            return res.status(400).send({ 
+                message : "No user is registerd with this email."
+            }); 
+        } 
+        else { 
+            if (user.validPassword(req.body.password)) { 
+                return res.status(201).send({ 
+                    message : "User Logged In Successfully", 
+                }) 
+            } 
+            else { 
+                return res.status(400).send({ 
+                    message : "Wrong Password or email. please try again"
+                }); 
+            } 
+        } 
+    }); 
+}); 
 
 
 module.exports = router;
