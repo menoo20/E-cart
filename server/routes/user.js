1bd193fe3eec9c5984a16e76743d9691468ae141
mohammed-ameen <menooteaching10@gmail.com>
2022-04-03 07:32:47 +0200

    added the user schema and tested saving in the db

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,4 @@
 const router = require("express").Router();
 
- router.get("/userTest", (req, res)=>{
-    res.send("router is working");
-})
-
-
-router.post("/postUser", (req, res)=>{
-    const username = req.body.username;
-    res.send(username)
-})
 
 module.exports = router
