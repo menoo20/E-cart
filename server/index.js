926047c439d0a7c4ae9f0bec461de65a4f5e7896
mohammed-ameen <menooteaching10@gmail.com>
2022-04-05 17:43:05 +0200

    added the post product route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product")
+
 
 dotenv.config();
  
@@ -15,7 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json()); 
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
-
+app.use("/api/products", productRoute)
 
 
 
