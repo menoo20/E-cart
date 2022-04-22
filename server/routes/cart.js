a6be323dde85795483ae70371d147db8b5ba61a6
AhmedMoO <ahmedmo.official@gmail.com>
2022-04-22 11:09:22 +0200

    cart and product finished&tested

---
+++ server/routes/cart.js
@@ -0,0 +1,115 @@
+
+const Cart = require("../models/Cart");
+const router = require("express").Router();
+
+//CREATE
+
+router.post("/", async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// add a products (and optional quantity) to cart of a specific user
+router.post("/:userId", async (req, res) => {
+  const newProductList = req.body.products;
+  
+  try {
+
+    newProductList.forEach(element => {
+
+      Cart.findOneAndUpdate(
+        {userId: req.params.userId}, 
+        { $push: { products:  element} }
+      );
+    });
+
+    const myCart = await Cart.findOne({ userId: req.params.userId });
+    
+    res.status(200).json(myCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a product from cart products
+router.delete("/:userId/:productId", async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    console.log(cart._id);
+    await Cart.updateOne({ _id: cart._id }, {
+      $pull: {
+          products: {productId: req.params.productId},
+      },
+  });
+    
+    res.status(200).json(`product id= ${req.params.productId} deleted`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//GET USER CART
+router.get("/find/:userId", async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+
+/*
+const itemId = 2;
+const query = {
+  item._id: itemId 
+};
+Person.findOne(query).then(doc => {
+  item = doc.items.id(itemId );
+  item["name"] = "new name";
+  item["value"] = "new value";
+  doc.save();
+
+  //sent respnse to client
+}).catch(err => {
+  console.log('Oh! Dark')
+});
+*/
---
+++ server/routes/product.js
@@ -0,0 +1,64 @@
+const Product = require("../models/Product");
+const router = require("express").Router();
+
+//CREATE
+
+router.post("/", async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get all products
+router.get("/", (req, res) => {
+  Product.find({}, (err, result) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json(result);
+      }
+  });
+});
+
+module.exports = router;
