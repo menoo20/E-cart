0efeaf96b6172c5c10c9dc898b034e9a855c3407
mohammed-ameen <menooteaching10@gmail.com>
2022-04-26 20:05:02 +0200

    repeated changes to the navbar pt2

--- client/src/components/NavSearchInput.jsx
+++ client/src/components/NavSearchInput.jsx
@@ -12,6 +12,11 @@ const Input = styled.input`
    box-shadow:none
  }
 `
+const Span = styled.span`
+ &.input-group-text{
+     padding: 0
+ }
+`
 
 const NavSearchInput = () => {
 const [categoryName, setCategoryName] = useState("Category")
@@ -22,18 +27,25 @@ const handleClick = (e)=>{
 
 }
   return (
-    <div className="input-group">
-        <Button className="btn btn-primary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">{categoryName==="All"? "Category": categoryName}</Button>
-        <ul className="dropdown-menu " onClick={e => handleClick(e)}>
-            {categoryName === "All" || categoryName === "Category" ? "" :<li><a className="dropdown-item" href="#" value="All" >All</a></li>}
-            <li><a className="dropdown-item" href="#" value="Vegetables" >Vegetables</a></li>
-            <li><a className="dropdown-item" href="#" value="Fruits">Fruits</a></li>
-            <li><a className="dropdown-item" href="#" value="Bread">Bread</a></li>
-        </ul>
-        <Input type="text" className="form-control " 
-        aria-label="Text input with dropdown button"
-        placeholder='Product name'></Input>
-    </div>
+    <form action="" className='d-flex me-2 '>
+        <div className="input-group">
+            <Button className="btn btn-primary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">{categoryName==="All"? "Category": categoryName}</Button>
+            <ul className="dropdown-menu " onClick={e => handleClick(e)}>
+                {categoryName === "All" || categoryName === "Category" ? "" :<li><a className="dropdown-item" href="#" value="All" >All</a></li>}
+                <li><a className="dropdown-item" href="#" value="Vegetables" >Vegetables</a></li>
+                <li><a className="dropdown-item" href="#" value="Fruits">Fruits</a></li>
+                <li><a className="dropdown-item" href="#" value="Bread">Bread</a></li>
+            </ul>
+            <Input type="text" className="form-control border-bottom-0" 
+            aria-label="Text input with dropdown button"
+            placeholder='Product name'></Input>
+            {/* <Span className="input-group-text submit-text" id="basic-addon1 p-0"> */}
+                <button className='btn btn-primary rounded-end input-group-text' type='submit'>
+                    <i className='bi bi-search'></i>
+                </button>
+            {/* </Span> */}
+        </div>
+    </form>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,8 +4,7 @@ import styled from 'styled-components';
 import NavSearchInput from './NavSearchInput';
 import SearchByCat from './SearchByCat';
 import userPlaceHolder from "../images/3177440.png"
-
-
+import "../style/navbar.scss"
 
 
 const Navbar = () => {
@@ -16,10 +15,10 @@ const Navbar = () => {
           <h2 className='mb-0'> <i className='bi bi-cart4'></i> e-cart</h2>
         </Link>
  
-        <button class="navbar-toggler order-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse mb-2 mb-lg-0 justify-content-center" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
@@ -31,15 +30,13 @@ const Navbar = () => {
             <a className="nav-link" href="#">Contact us</a>
           </li>
           <li className='nav-item'>
-            <form action="" className='d-flex me-2 '>
               <NavSearchInput/>
-            </form>
           </li>
         </ul>
       </div>
 
-      
-      <div className='me-2 order-4'>
+
+      <div className='me-2 d-none d-lg-flex'>
           <div className='d-flex align-self-center'>
             <img className='cursor-pointer' src="https://img.icons8.com/color/40/000000/add-shopping-cart--v1.png"/>
             <div className='rounded-circle px-3'>
@@ -47,6 +44,16 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+
+        <div className='me-2 d-lg-none'>
+          <div className='d-flex align-self-center'>
+            <img className='cursor-pointer' src="https://img.icons8.com/color/40/000000/add-shopping-cart--v1.png"/>
+            <div className='rounded-circle px-3'>
+              <img className='img-fluid' src={userPlaceHolder} alt="avatar" width={'40'} />
+            </div>
+          </div>
+        </div>
+        
 
       </div>
     </nav>
